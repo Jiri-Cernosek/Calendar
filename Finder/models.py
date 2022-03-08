18a5cb68784f5e9b0e3d373b9333d0f4f6ef6b0e
Jiri-Cernosek <97969046+Jiri-Cernosek@users.noreply.github.com>
2022-03-08 16:41:03 +0100

    Add files via upload

--- Finder/models.py
+++ Finder/models.py
@@ -1,17 +1,39 @@
 from django.db import models
 
 class SalesManager(models.Model):
-    ManagerName = models.CharField(max_length = 30)
+    manager_name = models.CharField(max_length = 30)
+
+    def __str__(self):
+        return "{0}".format(self.manager_name)
+
+    class Meta:
+        verbose_name="Manager"
+        verbose_name_plural="Managers"
+
 
 class PostalCode(models.Model):
-    City = models.CharField(max_length = 20)
-    District = models.CharField(max_length = 20)
-    Region = models.CharField(max_length = 20)
-    Code = models.IntegerField()
+    city = models.CharField(max_length = 20)
+    district = models.CharField(max_length = 20)
+    region = models.CharField(max_length = 20)
+    code = models.IntegerField()
+
+    def __str__(self):
+        return "City: {0} | Postal code: {1}".format(self.city, self.code)
+
+    class Meta:
+        verbose_name="Postal code"
+        verbose_name_plural="Postal codes"
+
 
 class Customer(models.Model):
-    CustomerManager = models.ForeignKey(SalesManager, on_delete = models.CASCADE)
-    CustomerName = models.CharField(max_length = 50)
-    CustomerPostalCode = models.ForeignKey(PostalCode, on_delete = models.CASCADE)
-    CustomerAddress = models.CharField(max_length = 250)
+    manager = models.ForeignKey(SalesManager, on_delete = models.CASCADE)
+    name = models.CharField(max_length = 50)
+    postal_code = models.ForeignKey(PostalCode, on_delete = models.RESTRICT)
+    address = models.CharField(max_length = 250)
 
+    def __str__(self):
+        return "{0}".format(self.name)
+
+    class Meta:
+        verbose_name="Customer"
+        verbose_name_plural="Customers"
